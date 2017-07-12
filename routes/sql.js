5829ad69884b2e16d3f3c49c8beb89f77b697041
Sgearjhz <624881768@qq.com>
2017-07-12 16:48:39 +0800

    用于连接数据库

--- routes/sql.js
+++ routes/sql.js
@@ -4,7 +4,7 @@ var mysql  = require('mysql');
 var connection = mysql.createConnection({                                                        
     host: 'localhost',                                                                      
     user: 'root',                                                                           
-    password: 'Qwer1234a',                                                                      
+    password: '数据库密码',                                                                      
     port: '3306',                                                                                
     database: 'game',                                                                            
 });
